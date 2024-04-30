8901b8f43549c0c76d0c38d16d7b9bd13c5b2d7f
pauloavm <pauloavmunhoz@gmail.com>
2024-04-30 15:31:04 -0300

    Lazy Load
    Lazy Load em JavaScript Vanilla.
    site em Html e Css3

---
+++ Lazy_Loader/js/scripts.js
@@ -0,0 +1,15 @@
+const images = document.querySelectorAll(".image-container img");
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) return;
+    const image = entry.target;
+    image.src = image.src.replace("80&w=10&", "80&w=1000&");
+
+    observer.unobserve(image);
+  });
+});
+
+images.forEach((image) => {
+  observer.observe(image);
+});
