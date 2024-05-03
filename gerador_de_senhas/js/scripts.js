4d152ea1e89b6edbfdad5be156eb671c1228b44f
pauloavm <pauloavmunhoz@gmail.com>
2024-05-03 13:38:02 -0300

    Update scripts.js

--- gerador_de_senhas/js/scripts.js
+++ gerador_de_senhas/js/scripts.js
@@ -12,3 +12,33 @@ const lettersInput = document.querySelector("#letters");
 const numbersInput = document.querySelector("#numbers");
 const symbolsInput = document.querySelector("#symbols");
 const copyPasswordButton = document.querySelector("#copy-password");
+
+
+// Funções
+const getLetterLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  };
+  
+  const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  };
+  
+  const getNumber = () => {
+    return Math.floor(Math.random() * 11).toString();
+  };
+  
+  const getSymbol = () => {
+    const symbols = "(){}[]=<>/,.!@#$%^&*";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+  };
+  
+  const generatePassword = (
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol
+  ) => {
+    let password = "";
+  
+    const passwordLength = lengthInput.value;
+  
