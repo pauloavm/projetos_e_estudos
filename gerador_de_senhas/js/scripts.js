46a55e547963b4e4cc4ab365fa8a39084b167aef
pauloavm <pauloavmunhoz@gmail.com>
2024-05-03 13:37:36 -0300

    Update scripts.js

--- gerador_de_senhas/js/scripts.js
+++ gerador_de_senhas/js/scripts.js
@@ -1,3 +1,14 @@
 // Seleção de elementos
 const generatePasswordButton = document.querySelector("#generate-password");
 const generatedPasswordElement = document.querySelector("#generated-password");
+
+// Após refatoração
+const openCloseGeneratorButton = document.querySelector(
+  "#open-generate-password"
+);
+const generatePasswordContainer = document.querySelector("#generate-options");
+const lengthInput = document.querySelector("#length");
+const lettersInput = document.querySelector("#letters");
+const numbersInput = document.querySelector("#numbers");
+const symbolsInput = document.querySelector("#symbols");
+const copyPasswordButton = document.querySelector("#copy-password");
