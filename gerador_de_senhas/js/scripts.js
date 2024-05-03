495bf71270a94b1e32c28b340c7685a545192793
pauloavm <pauloavmunhoz@gmail.com>
2024-05-03 13:37:03 -0300

    Gerador de senhas

---
+++ gerador_de_senhas/js/scripts.js
@@ -0,0 +1,3 @@
+// Seleção de elementos
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
