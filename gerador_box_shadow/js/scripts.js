9a2bf1ddf04b52a68b8b1cc3118d2f2b289aa842
pauloavm <pauloavmunhoz@gmail.com>
2024-05-04 14:17:01 -0300

    Update scripts.js

--- gerador_box_shadow/js/scripts.js
+++ gerador_box_shadow/js/scripts.js
@@ -106,3 +106,47 @@ class BoxShadowGenerator {
   }
 }
 
+// Selecionar elementos
+const horizontal = document.querySelector("#horizontal");
+const horizontalRef = document.querySelector("#horizontal-value");
+const vertical = document.querySelector("#vertical");
+const verticalRef = document.querySelector("#vertical-value");
+const blur = document.querySelector("#blur");
+const blurRef = document.querySelector("#blur-value");
+const spread = document.querySelector("#spread");
+const spreadRef = document.querySelector("#spread-value");
+const previewBox = document.querySelector("#box");
+
+const color = document.querySelector("#color");
+const colorRef = document.querySelector("#color-value");
+
+const opacity = document.querySelector("#opacity");
+const opacityRef = document.querySelector("#opacity-value");
+
+const inset = document.querySelector("#inset");
+
+const rule = document.querySelector("#rule span");
+const webkitRule = document.querySelector("#webkit-rule span");
+const mozRule = document.querySelector("#moz-rule span");
+
+const boxShadow = new BoxShadowGenerator(
+  horizontal,
+  horizontalRef,
+  vertical,
+  verticalRef,
+  blur,
+  blurRef,
+  spread,
+  spreadRef,
+  color,
+  colorRef,
+  opacity,
+  opacityRef,
+  inset,
+  previewBox,
+  rule,
+  webkitRule,
+  mozRule
+);
+
+boxShadow.initialize();
