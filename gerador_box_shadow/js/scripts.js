55178933d409affd5ced0a42ea1e79594e13b3bd
pauloavm <pauloavmunhoz@gmail.com>
2024-05-04 14:15:51 -0300

    Update scripts.js

--- gerador_box_shadow/js/scripts.js
+++ gerador_box_shadow/js/scripts.js
@@ -1,1 +1,39 @@
-
+class BoxShadowGenerator {
+    constructor(
+      horizontal,
+      horizontalRef,
+      vertical,
+      verticalRef,
+      blur,
+      blurRef,
+      spread,
+      spreadRef,
+      color,
+      colorRef,
+      opacity,
+      opacityRef,
+      inset,
+      previewBox,
+      rule,
+      webkitRule,
+      mozRule
+    ) {
+      this.horizontal = horizontal;
+      this.horizontalRef = horizontalRef;
+      this.vertical = vertical;
+      this.verticalRef = verticalRef;
+      this.blur = blur;
+      this.blurRef = blurRef;
+      this.spread = spread;
+      this.spreadRef = spreadRef;
+      this.color = color;
+      this.colorRef = colorRef;
+      this.opacity = opacity;
+      this.opacityRef = opacityRef;
+      this.inset = inset;
+      this.insetRef = inset.checked;
+      this.previewBox = previewBox;
+      this.rule = rule;
+      this.webkitRule = webkitRule;
+      this.mozRule = mozRule;
+    }
