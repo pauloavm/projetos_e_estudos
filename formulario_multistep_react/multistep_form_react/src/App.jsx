702757069d1a3bcbcbe5f86d1cb29f5836cdc238
pauloavm <pauloavmunhoz@gmail.com>
2024-05-08 10:40:58 -0300

    Update App.jsx

--- formulario_multistep_react/multistep_form_react/src/App.jsx
+++ formulario_multistep_react/multistep_form_react/src/App.jsx
@@ -1,9 +1,33 @@
+// components
+import { GrFormNext, GrFormPreviuos } from "react-icons/gr";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Multistep</h1>
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com sua compra, utilize o formulário abaixo para
+          avaliar o produto.
+        </p>
+      </div>
+      <div className="form-container">
+        <p>etapas</p>
+        <form>
+          <div className="actions">
+            <button type="button">
+              <GrFormPreviuos />
+              <span>Voltar</span>
+            </button>
+            <button type="submit">
+              <GrFormNext />
+              <span>Avançar</span>
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
