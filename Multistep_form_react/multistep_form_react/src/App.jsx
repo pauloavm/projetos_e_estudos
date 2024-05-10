5a3aaeb09a44ff406e48094a24ec4cb204505790
pauloavm <pauloavmunhoz@gmail.com>
2024-05-10 12:11:39 -0300

    criação dos arquivos iniciais

---
+++ Multistep_form_react/multistep_form_react/src/App.jsx
@@ -0,0 +1,37 @@
+// Components
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <div className="header">
+          <h2>Deixe sua avaliação</h2>
+          <p>
+            Estamos felizes com sua compra! Utilize o formulário para avaliar o
+            produto
+          </p>
+          <div className="form-container">
+            <p>etapas</p>
+            <form action="">
+              <div className="actions">
+                <button type="button">
+                  <GrFormPrevious /> <span>Voltar</span>
+                </button>
+                <button type="submit">
+                  {" "}
+                  <span>Avançar</span>
+                  <GrFormNext />
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
