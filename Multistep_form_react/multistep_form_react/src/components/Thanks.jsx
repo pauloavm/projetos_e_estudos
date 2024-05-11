d31b3f55f8b49cde78a84310814df0ec5207834a
pauloavm <pauloavmunhoz@gmail.com>
2024-05-11 10:30:01 -0300

    .

--- Multistep_form_react/multistep_form_react/src/components/Thanks.jsx
+++ Multistep_form_react/multistep_form_react/src/components/Thanks.jsx
@@ -9,8 +9,20 @@ import {
 
 const Thanks = () => {
   return (
-    <div>
-      <h2>Thanks</h2>
+    <div className="thnaks-container">
+      <h2>Falta pouco...</h2>
+      <p>
+        A sua opinião é muito importante, em breve você receberá um cupom de
+        desconto para sua proxima compra.
+      </p>
+      <p>Para concluir sua anavliação clique em enviar.</p>
+      <h3>Aqui está um resumo de sua avaliação:</h3>
+      <p className="review-data">
+        <span>Satisfação com o produto:</span>
+      </p>
+      <p className="review-data">
+        <span>Comentário:</span>
+      </p>
     </div>
   );
 };
