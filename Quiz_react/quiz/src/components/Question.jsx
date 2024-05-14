f00e285d907bf258397db36021bc6450de97e4a2
pauloavm <pauloavmunhoz@gmail.com>
2024-05-14 13:11:43 -0300

    .

---
+++ Quiz_react/quiz/src/components/Question.jsx
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
+const Question = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
+  console.log(quizState);
+  return <div>Chupa</div>;
+};
+
+export default Question;
