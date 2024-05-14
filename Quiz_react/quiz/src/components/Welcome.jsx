3df0c676e2916e1298f9511e96d20858821702cc
pauloavm <pauloavmunhoz@gmail.com>
2024-05-14 12:53:07 -0300

    .

--- Quiz_react/quiz/src/components/Welcome.jsx
+++ Quiz_react/quiz/src/components/Welcome.jsx
@@ -1,11 +1,19 @@
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
 import Quiz from "../img/quiz.svg";
 import "../components/Welcome.css";
+
 const Welcome = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
+
   return (
     <div id="welcome">
       <h2>Seja bem vindo</h2>
       <p>Clique no botão abaixo para começão</p>
-      <button>Iniciar</button>
+      <button onClick={() => dispatch({ type: "CHANCE_STATE" })}>
+        Iniciar
+      </button>
       <img src={Quiz} alt="Inicio do Quiz" />
     </div>
   );
