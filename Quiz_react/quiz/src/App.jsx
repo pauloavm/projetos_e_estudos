8de78db7c2ecb5510279cab516655d1a6c41ef45
pauloavm <pauloavmunhoz@gmail.com>
2024-05-14 13:50:22 -0300

    .

--- Quiz_react/quiz/src/App.jsx
+++ Quiz_react/quiz/src/App.jsx
@@ -3,24 +3,23 @@ import { QuizContext } from "./context/quiz";
 
 import Welcome from "./components/Welcome";
 import Question from "./components/Question";
+import GameOver from "./components/GameOver";
+
+import PickCategory from "./components/PickCategory";
 
 import "./App.css";
 
 function App() {
   const [quizState, dispatch] = useContext(QuizContext);
 
-  useEffect(() => {
-    dispatch({ type: "REORDER_QUESTIONS" });
-  }, []);
-
   return (
-    <>
-      <div className="App">
-        <h1>Quiz Programação</h1>
-        {quizState.gameStage === "Start" && <Welcome />}
-        {quizState.gameStage === "Start" && <Question />}
-      </div>
-    </>
+    <div className="App">
+      <h1>Quiz de Programação</h1>
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Category" && <PickCategory />}
+      {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
+    </div>
   );
 }
 
--- Quiz_react/quiz/src/main.jsx
+++ Quiz_react/quiz/src/main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
+import { QuizProvider } from "./context/quiz";
 import "./index.css";
-
-import { QuizProvider } from "./context/quiz.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
