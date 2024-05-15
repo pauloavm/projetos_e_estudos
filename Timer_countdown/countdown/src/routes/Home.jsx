310eaf644f9be98b002618abcc2d880297bd28f8
pauloavm <pauloavmunhoz@gmail.com>
2024-05-15 14:36:49 -0300

    Update Home.jsx

--- Timer_countdown/countdown/src/routes/Home.jsx
+++ Timer_countdown/countdown/src/routes/Home.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <h2>Monte sua contagem regressiva</h2>
+      <form className="countdown-form">
+        <label>
+          <span>Titulo:</span>
+          <input type="text" name="title" placeholder="Título do evento" />
+        </label>
+        <label>
+          <span>Data do evento :</span>
+          <input type="date" name="date"  />
+        </label>
+        <label>
+          <span>Imagem:</span>
+          <input type="text" name="image" placeholder="Insira a url da imagem" />
+        </label>
+      </form>
+        <label>
+          <span>Cor do tema:</span>
+          <input type="color" name="color" />
+        </label>
+        <input type="submit" value="Enviar" />
+        </form>
+    </div>
+   
+);
+};
 
-export default Home
+export default Home;
