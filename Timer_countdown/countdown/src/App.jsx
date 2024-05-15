8a4e7fd2162a46494eeaab5b22ec26f5d10dee5e
pauloavm <pauloavmunhoz@gmail.com>
2024-05-15 15:46:15 -0300

    Update App.jsx

--- Timer_countdown/countdown/src/App.jsx
+++ Timer_countdown/countdown/src/App.jsx
@@ -1,15 +1,27 @@
-
 import { Outlet } from "react-router-dom";
 
+import { useContext } from "react";
+import { CountdownContext } from "./context/CountdownContext";
+
 import Volcano from "./assets/volcano.jpg";
-
-
 
 import "./App.css";
 
 function App() {
+  const { event } = useContext(CountdownContext);
+
+  let eventImage = null;
+  if (event) eventImage = event.image;
+
   return (
-    <div className="App" style={{ backgroundImage: `url(${Volcano})` }}>
+    <div
+      className="App"
+      style={
+        eventImage
+          ? { backgroundImage: `url(${eventImage})` }
+          : { backgroundImage: `url(${Volcano})` }
+      }
+    >
       <div className="container">
         <Outlet />
       </div>
