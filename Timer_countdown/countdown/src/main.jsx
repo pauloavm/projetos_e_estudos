e001f8ddcda87d3792d768a36296c5e6c957620b
pauloavm <pauloavmunhoz@gmail.com>
2024-05-15 15:19:41 -0300

    .

--- Timer_countdown/countdown/src/main.jsx
+++ Timer_countdown/countdown/src/main.jsx
@@ -9,6 +9,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/Home.jsx";
 import Countdown from "./routes/Countdown.jsx";
 
+// context
+import { CountdownProvider } from "./context/CountdownContext.jsx";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -28,6 +31,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <CountdownProvider>
     <RouterProvider router={router} />
+    </CountdownProvider>
   </React.StrictMode>
 );
