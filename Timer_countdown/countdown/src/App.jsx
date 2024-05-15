61fb199b3861c4a7a82f1917d59bffb77aa48a4d
pauloavm <pauloavmunhoz@gmail.com>
2024-05-15 13:56:40 -0300

    lógica do counter

--- Timer_countdown/countdown/src/App.jsx
+++ Timer_countdown/countdown/src/App.jsx
@@ -1,13 +1,17 @@
 import Title from "./components/Title";
 import Counter from "./components/Counter";
 
-import Volcano from './assets/volcano.jpg'
+import Volcano from "./assets/volcano.jpg";
+
+import useCountdown from "./hooks/useContdown";
 
 import "./App.css";
 
 function App() {
+  const x = useCountdown("Jan 1, 2025 00:00:00");
+
   return (
-    <div className="App" style={{backgroundImage: `url(${Volcano})`}} >
+    <div className="App" style={{ backgroundImage: `url(${Volcano})` }}>
       <div className="container">
         <Title title="Contagem regressiva para 2025" />
         <div className="countdown-container">
