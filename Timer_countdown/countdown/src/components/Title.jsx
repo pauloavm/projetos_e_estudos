4838dcda7830a2f8d0f2bd077a3897d97dc9e75f
pauloavm <pauloavmunhoz@gmail.com>
2024-05-15 15:59:13 -0300

    .

--- Timer_countdown/countdown/src/components/Title.jsx
+++ Timer_countdown/countdown/src/components/Title.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Title.css";
 
-const Title = ({ title }) => {
-  return <h1 className="tltle">{title}</h1>;
+const Title = ({ title, eventColor }) => {
+  return (
+    <h1 className="tltle" style={{ color: eventColor }}>
+      {title}
+    </h1>
+  );
 };
 
 export default Title;
