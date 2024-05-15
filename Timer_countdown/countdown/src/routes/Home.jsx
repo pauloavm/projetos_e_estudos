f3db34333b14851698b3b334629d61dd6e27c8a5
pauloavm <pauloavmunhoz@gmail.com>
2024-05-15 15:04:25 -0300

    Update

--- Timer_countdown/countdown/src/routes/Home.jsx
+++ Timer_countdown/countdown/src/routes/Home.jsx
@@ -1,32 +1,62 @@
 import React from "react";
+import { useState } from "react";
+import "./Home.css";
 
 const Home = () => {
+  const [title, setTitle] = useState();
+  const [date, setDate] = useState();
+  const [image, setImage] = useState();
+  const [color, setColor] = useState();
+
+
+  
+
   return (
     <div className="home">
       <h2>Monte sua contagem regressiva</h2>
-      <form className="countdown-form">
+      <form className="countdown-form" onSubmit={handleSubmit}>
         <label>
           <span>Titulo:</span>
-          <input type="text" name="title" placeholder="Título do evento" />
+          <input
+            type="text"
+            name="title"
+            placeholder="Título do evento"
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
         </label>
         <label>
           <span>Data do evento :</span>
-          <input type="date" name="date"  />
+          <input
+            type="date"
+            name="date"
+            onChange={(e) => setDate(e.target.value)}
+            required
+          />
         </label>
         <label>
           <span>Imagem:</span>
-          <input type="text" name="image" placeholder="Insira a url da imagem" />
+          <input
+            type="text"
+            name="image"
+            placeholder="Insira a url da imagem"
+            onChange={(e) => setImage(e.target.value)}
+          />
         </label>
-      </form>
+
         <label>
           <span>Cor do tema:</span>
-          <input type="color" name="color" />
+          <input
+            type="color"
+            name="color"
+            onChange={(e) => setColor(e.target.value)}
+            required
+          />
         </label>
         <input type="submit" value="Enviar" />
-        </form>
+      </form>
     </div>
-   
-);
+  );
 };
 
 export default Home;
