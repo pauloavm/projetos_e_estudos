8d83ae90ada7dcde9a786b6c28dd184513f06a28
pauloavm <pauloavmunhoz@gmail.com>
2024-05-16 14:47:51 -0300

    .

--- github_finder/src/components/Search.tsx
+++ github_finder/src/components/Search.tsx
@@ -5,13 +5,15 @@ type SearchProps = {
 import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 
+import classes from './Search.module.css'
+
 const Search = ({ loadUser }: SearchProps) => {
   const [userName, setUserName] = useState("");
   return (
-    <div>
+    <div className= {classes.search}>
       <h2>Busque por um usuário</h2>
       <p>Conheça seus melhores repositórios</p>
-      <div>
+      <div className={classes.search_container} >
         <input
           type="text"
           placeholder="Digite o nome do usuário"
