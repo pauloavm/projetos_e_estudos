662fc4b054d594cf2d720b80dcdb59b68166d06b
pauloavm <pauloavmunhoz@gmail.com>
2024-05-16 14:05:36 -0300

    .

--- github_finder/src/App.tsx
+++ github_finder/src/App.tsx
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <h1>GitHub Finder</h1>
+      <Outlet/>
     </div>
   );
 }
---
+++ github_finder/src/components/Search.tsx
@@ -0,0 +1,21 @@
+type SearchProps = {
+  loadUser: (username: string) => Promise<void>,
+};
+import { BsSearch } from "react-icons/bs";
+
+const Search = ({loadUser}: SearchProps) => {
+  return (
+    <div>
+      <h2>Busque por um usuário</h2>
+      <p>Conheça seus melhores repositórios</p>
+      <div>
+        <input type="text" placeholder="Digite o nome do usuário" />
+        <button>
+          <BsSearch />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
